350521ab74cb7005f42a412c5e15cc523135c233
davebro1031 <dwbroaddus@gmail.com>
2023-08-15 13:42:06 -0600

    create loginpage for mobile view, add sidebar to page structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import {Routes, Route} from "react-router-dom"
 import NewUser from './components/NewUser'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import LoginPage from './components/LoginPage'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
   <>
     <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/newuser" element={<NewUser/>}/>
-    </Routes>
+    <div id="content">
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/newuser" element={<NewUser/>}/>
+        <Route path="login" element={<LoginPage/>}/>
+      </Routes>
+    </div>
   </>
   )
 }
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,60 @@
+import {useState} from "react"
+import {Link} from "react-router-dom"
+
+const BASE_URL = `${import.meta.env.VITE_STRANGERS_THINGS_BASE_API}`
+
+
+export default function LoginPage(){
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function loginUser(event){
+        try {
+            event.preventDefault();
+            const response = await fetch(`${BASE_URL}/users/login`, {
+              method: "POST",
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                user: {
+                  username: username,
+                  password: password
+                }
+              })
+            });
+            const result = await response.json();
+            console.log(result);
+
+            localStorage.setItem("token", result.data.token)
+            localStorage.setItem("user", username)
+
+            return result
+          } catch (err) {
+            console.error(err);
+          }
+    }
+
+    return(
+    
+    <div id="login-page">
+        <form id="login-page-form" onSubmit={(event)=>loginUser(event)}>
+
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input className="login-input" type="text" id="username" name = "username" value ={username} onChange={(event)=>setUsername(event.target.value)} />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input className="login-input" type="password" id="password" name="password" value ={password} onChange={(event)=>setPassword(event.target.value)} />
+            </div>
+
+            <input id = "login-button" type="submit" value="Log In" />
+        </form>
+        <Link to="/newuser">Don't have an account?</Link>
+    
+    </div>
+    )
+}
