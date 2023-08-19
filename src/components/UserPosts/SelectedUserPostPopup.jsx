95ce386edff5b6aaf458839543fcabff5c64a50e
davebro1031 <dwbroaddus@gmail.com>
2023-08-18 21:53:28 -0600

    pull current GMT time from timedate API

--- src/components/UserPosts/SelectedUserPostPopup.jsx
+++ src/components/UserPosts/SelectedUserPostPopup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 function SelectedUserPostPopup({trigger, setTrigger, id, title, description, price, deliver, created, updated, location}) {
@@ -12,17 +12,60 @@ function SelectedUserPostPopup({trigger, setTrigger, id, title, description, pri
         return {month: monthProp, day: dayProp, hour: hourProp, minute: minuteProp}
     }
 
-    if(trigger){
-        const date = new Date()
+    function compareDates(date1, date2){
+        let days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        let difference = {
+            month: date2.month - date1.month,
+            day: date2.day - date1.day,
+            hour: date2.hour - date1.hour,
+            minute: date2.minute - date1.minute
+        }
 
-        const dateCurr = {month: date.getMonth() + 1, day: date.getDate(), hour: date.getHours(), minute: date.getMinutes()}
+        if(difference.minute < 0){
+            difference.minute += 60
+            difference.hour -= 1
+        }
 
-        console.log(dateCurr)
-        console.log(parseDateProp(created))
-        console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
-        console.log(date.getTimezoneOffset())
+        if(difference.hour < 0){
+            difference.hour += 24
+            difference.day -= 1
+        }
+
+        return difference.minute
 
     }
+
+    const [currentGMT, setCurrentGMT]=useState(null)
+    async function getCurrentGMT(){
+        try{
+            const response = await fetch("http://api.timezonedb.com/v2.1/get-time-zone?key=Z1CROW4937CK&format=json&by=zone&zone=Africa/Abidjan")
+            const result = await response.json()
+            // console.log(result.formatted)
+            setCurrentGMT(result.formatted)
+        }catch(err){
+            console.error(err)
+        }
+    }
+
+    useEffect(()=>{
+        if(trigger){
+            getCurrentGMT()
+        }
+    },[trigger])
+
+    // useEffect(()=>{
+    //     if(trigger){
+    //         const date = new Date()
+    
+    //         const dateCurr = {month: date.getMonth() + 1, day: date.getDate(), hour: date.getHours(), minute: date.getMinutes()}
+    
+    //         console.log(dateCurr)
+
+    //         console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
+    //         console.log(date.getTimezoneOffset())
+    //     }
+
+    // },[])
     
 
     return (
@@ -38,7 +81,7 @@ function SelectedUserPostPopup({trigger, setTrigger, id, title, description, pri
                             <div>Created at: {created}</div>
                             <div>Updated at: {updated}</div>
                             <div>Location: {location}</div>
-                            {/* <div>Current Time: {time}</div> */}
+                            {currentGMT?<div>Current GMT: {compareDates(parseDateProp(created), parseDateProp(currentGMT))}</div>:null}
                         </div>
                     </div>
                 </Link>
