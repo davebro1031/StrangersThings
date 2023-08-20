55740ef4d2befc5ca919a9bdb52dbd3646766c6a
orbitalben <orbitaben@gmail.com>
2023-08-20 10:36:07 -0400

    took code from master to resolve pull issues

--- src/App.jsx
+++ src/App.jsx
@@ -43,23 +43,6 @@ function App() {
           {" "}
           <footer>footer</footer>
         </div>
-      <Navbar />
-      <div id="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/newuser" element={<NewUser />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="messages" element={<MessageSender />} />
-          <Route path="posts" element={<Posts/>}/>
-          <Route path="" element={<Posts query={query}/>} />
-          <Route path="newuser" element={<NewUser/>} />
-          <Route path="login" element={<LoginPage/>} />
-          <Route path="messages" element={<Messages query={query}/>} />
-          <Route path="userposts" element={<UserPosts query={query}/>} />
-          <Route path="*" element={<h2>Route not found</h2>} />
-          <Route path="single-details" element={<SinglePlayer/>}/>
-          {/* <Route path="posts" element={<Posts />} /> */}
-        </Routes>
       </div>
 
       {/*   
