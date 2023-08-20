61853dce7e61a30912e5a4f695b2469db1afd579
AssClapios <tylermjohnson1991@gmail.com>
2023-08-20 16:33:33 -0600

    navbar

--- src/src/src/Navigation/Navbar.js
+++ src/src/src/Navigation/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="NavBar">
       Navbar
     </a>
     <button
