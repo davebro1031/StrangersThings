5b7108ab456291d59337aabb2777a1cec3d88130
Fred Genske <f_genske@hotmail.com>
2023-08-19 20:25:44 -0600

    refactor:sidebar display

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,45 @@ import Sidebar from "./components/Access/Sidebar";
 import UserPosts from "./components/UserPosts/UserPosts";
 
 function App() {
-
-  const sidebarPathnames = ["/", "/messages", "/userposts"]
-  const [query, setQuery] = useState("")
-  const location=useLocation()
+  const sidebarPathnames = ["/", "/messages", "/userposts"];
+  const [query, setQuery] = useState("");
+  const location = useLocation();
 
   return (
     <>
-      <Header />
-      <div id="content">
-        {sidebarPathnames.includes(location.pathname)?<Sidebar query={query} setQuery={setQuery}/>:null}
-        <Routes>
-          <Route path="" element={<Posts query={query}/>} />
-          <Route path="newuser" element={<NewUser/>} />
-          <Route path="login" element={<LoginPage/>} />
-          <Route path="messages" element={<Messages query={query}/>} />
-          <Route path="userposts" element={<UserPosts query={query}/>} />
-          <Route path="*" element={<h2>Route not found</h2>} />
-          {/* <Route path="posts" element={<Posts />} /> */}
-        </Routes>
+      <div className="contianer">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="content-container">
+          <aside>
+            {sidebarPathnames.includes(location.pathname) ? (
+              <Sidebar query={query} setQuery={setQuery} />
+            ) : null}
+          </aside>
+          <main>
+            <Routes>
+              <Route path="" element={<Posts query={query} />} />
+              <Route path="newuser" element={<NewUser />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="messages" element={<Messages query={query} />} />
+              <Route path="userposts" element={<UserPosts query={query} />} />
+              <Route path="posts" element={<Posts />} />
+              <Route path="*" element={<h2>Route not found</h2>} />
+            </Routes>
+          </main>
+        </div>
+        <div className="footer">
+          {" "}
+          <footer>footer</footer>
+        </div>
       </div>
+
+      {/*   
+      <div id="content">
+
+
+      </div> */}
     </>
   );
 }
