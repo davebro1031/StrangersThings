8971e7c5fcf6d034229644e8d4a605b71d66916a
fredgenske <132321602+fredgenske@users.noreply.github.com>
2023-08-20 23:07:37 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Sidebar from "./components/Access/Sidebar";
 import UserPosts from "./components/UserPosts/UserPosts";
 import Selling from "./components/Selling/Selling";
 import NewSelling from "./components/Selling/NewSelling";
-
+return (<>
       <div id="content">
         {sidebarPathnames.includes(location.pathname)?<Sidebar query={query} setQuery={setQuery}/>:null}
         <Routes>
@@ -32,5 +32,5 @@ import NewSelling from "./components/Selling/NewSelling";
     </div>
     </>
   );
-}
+
 export default App;
