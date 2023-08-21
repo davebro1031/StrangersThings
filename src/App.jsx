55431465efae8cb564318ba5fed2539170deafc4
davebro1031 <dwbroaddus@gmail.com>
2023-08-20 23:09:00 -0600

    fixed old App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,21 @@ import Messages from "./components/Messaging/Messages";
 import Posts from "./components/Posts";
 import Sidebar from "./components/Access/Sidebar";
 import UserPosts from "./components/UserPosts/UserPosts";
+import MakePost from "./components/Posts/MakePost";
+import Navbar from "./components/Navigation/Navbar";
 
+
+
+function App() {
+
+  const sidebarPathnames = ["/", "/messages", "/userposts"]
+  const [query, setQuery] = useState("")
+  const location=useLocation()
+
+  return (
+    <>
+      <Header />
+     
       <div id="content">
         {sidebarPathnames.includes(location.pathname)?<Sidebar query={query} setQuery={setQuery}/>:null}
         <Routes>
@@ -16,11 +30,12 @@ import UserPosts from "./components/UserPosts/UserPosts";
           <Route path="login" element={<LoginPage/>} />
           <Route path="messages" element={<Messages query={query}/>} />
           <Route path="userposts" element={<UserPosts query={query}/>} />
+          <Route path="*" element={<h2>Route not found</h2>} />
           <Route path="makeposts" element={<MakePost/>}/>
           <Route path="Messages" element={<Navbar/>}/>
           <Route path="My posts" element={<Navbar/>}/>
           <Route path="All Listings" element={<Navbar/>}/>
-          <Route path="*" element={<h2>Route not found</h2>} />
+          {/* <Route path="posts" element={<Posts />} /> */}
         </Routes>
       
       </div>
